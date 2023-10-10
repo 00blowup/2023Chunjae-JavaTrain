d08b514c2fab6269bc69e7294b464158d337aa5e
00blowup <00blowup@gmail.com>
2023-10-10 15:42:12 +0900

    add: 필드는 없고 메소드만 있는 클래스 실습을 위한 코드 추가.

---
+++ Oct10/Calculator.java
@@ -0,0 +1,23 @@
+package Oct10;
+
+public class Calculator {
+    // 필드는 없고 메소드만 있는 클래스 실습
+
+    // 생성자
+    public Calculator () {
+    }
+
+    // 메소드들
+    void powerOn () {
+        System.out.println("전원을 켭니다.");
+    }
+    void powerOff () {
+        System.out.println("전원을 끕니다.");
+    }
+    int add (int a, int b) {
+        return a+b;
+    }
+    int sub (int a, int b) {
+        return a-b;
+    }
+}

9788afb5bdb43da6ae02c3bd02ae0740ae88f515
Haeun Noh <00blowup@gmail.com>
2023-10-11 16:26:13 +0900

    add: Override 실습을 위한 코드를 추가

---
+++ Oct11/OverrideParentClass.java
@@ -0,0 +1,9 @@
+package Oct11;
+
+public class OverrideParentClass {
+    // Override 실습을 위한 부모 클래스
+    public double areaCircle (double r) {
+        System.out.println("부모 클래스 메소드 실행");
+        return 3.14159 * r * r;
+    }
+}

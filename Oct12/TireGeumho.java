04abf441df43d601fac20dbed2634ebc6ab6261d
Haeun Noh <00blowup@gmail.com>
2023-10-12 16:19:20 +0900

    add: 다형성 실습을 위한 코드 추가

---
+++ Oct12/TireGeumho.java
@@ -0,0 +1,9 @@
+package Oct12;
+
+public class TireGeumho implements Tire{
+    // 다형성 예제를 실습하기 위한 타이어 자식 클래스2
+    @Override
+    public void roll() {
+        System.out.println("금호타이어! 구른다!");
+    }
+}
---
+++ Oct12/TireHankook.java
@@ -0,0 +1,9 @@
+package Oct12;
+
+public class TireHankook implements Tire{
+    // 다형성 예제를 실습하기 위한 타이어 자식 클래스1
+    @Override
+    public void roll() {
+        System.out.println("한국타이어! 구른다!");
+    }
+}
